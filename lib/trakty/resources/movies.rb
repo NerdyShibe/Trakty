485d5afe486f88d314a54b8ae9f4feebabdf998b
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-03 11:18:53 -0300

    feat: add some endpoints for the movies resource

---
+++ lib/trakty/resources/movies.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Trakty
+  module Resources
+    #
+    # Will handle endpoints for Movies
+    class Movies < Base
+      #
+      # Returns the most watched movies in the last 24h
+      # Movies with the most watchers are returned first
+      #
+      # Supports: Pagination + Extended Info + Filters
+      def trending
+        Trakty::RequestHandler.new(self, %w[movies trending])
+      end
+
+      #
+      # Returns the most popular movies. Popularity is calculated
+      # using the rating percentage and the number of ratings.
+      #
+      # Supports: Pagination + Extended Info + Filters
+      def popular
+        Trakty::RequestHandler.new(self, %w[movies popular])
+      end
+
+      #
+      # Returns the most favorited movies in the specified time period
+      #
+      # Supports: Pagination + Extended Info + Filters
+      def favorited
+        Trakty::RequestHandler.new(self, %w[movies favorited])
+      end
+
+      #
+      # Returns the most favorited movies in the specified time period
+      #
+      # Supports: Extended Info
+      def summary
+        Trakty::RequestHandler.new(self, %w[movies])
+      end
+    end
+  end
+end
