f0d1d9ef48478b6d3038447e09183224e8eea859
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-03 11:09:32 -0300

    build: update main module and extract version

--- lib/trakty.rb
+++ lib/trakty.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
-module Trakty
-  VERSION = '0.0.1'
-end
+require 'dotenv/load'
+require 'faraday'
+require 'json'
+
+require_relative 'trakty/auth'
+require_relative 'trakty/client'
+require_relative 'trakty/request_handler'
+require_relative 'trakty/version'
+
+require_relative 'trakty/resources/base'
+require_relative 'trakty/resources/movies'
+
+#
+# Trakt Rest API Client
+module Trakty; end
+
+Trakty::Client.new
