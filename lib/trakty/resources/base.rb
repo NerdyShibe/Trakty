8e5b9e0160b86cc860592543c2426380bb3dade1
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-04 16:01:00 -0300

    feat: delegate base resource methods to the client

--- lib/trakty/resources/base.rb
+++ lib/trakty/resources/base.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 module Trakty
   module Resources
     #
     # All other resources should inherit from this
     # Will handle the logic for the REST HTTP methods
     class Base
-      def initialize(client, id = nil)
+      extend Forwardable
+
+      def initialize(client)
         @client = client
-        @id = id
       end
 
-      def get(path, params = nil)
-        @client.connection.get(path, params)
-      end
-
-      # TODO: Implement the other HTTP methods
-      def post; end
-      def put; end
-      def delete; end
+      #
+      # DRY way of accessing the HTTP methods from the client
+      def_delegators :@client, :get, :post, :put, :delete
     end
   end
 end
